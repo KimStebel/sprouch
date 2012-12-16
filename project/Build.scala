2a755e0dc3e6658320848ad02906421a196a038b
Kim Stebel <mail@kim-stebel.com>
2012-12-16 15:14:17 +0100

    v0.5.2

--- project/Build.scala
+++ project/Build.scala
@@ -24,7 +24,7 @@ object ApplicationBuild extends Build {
         "gh-pages",
         new File("/home/k/workspaces/sprouch-pages/repository/")
     ))),
-    (version := "0.5.1")
+    (version := "0.5.2")
   ))
 
 }
