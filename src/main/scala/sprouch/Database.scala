9ee8dcae7f18e40b4aaececba91ab3d3aad2ad71
Kim Stebel <mail@kim-stebel.com>
2012-12-23 18:54:40 +0100

    scaladoc changes

--- src/main/scala/sprouch/Database.scala
+++ src/main/scala/sprouch/Database.scala
@@ -76,7 +76,7 @@ class Database private[sprouch](val name:String, pipelines:Pipelines) extends Ur
   }
   
   /**
-    * Retrives a document.
+    * Retrieves a document.
     */
   def getDoc[A:RootJsonFormat](id:String):Future[RevedDocument[A]] = {
     val p = pipeline[RevedDocument[A]]
@@ -84,7 +84,7 @@ class Database private[sprouch](val name:String, pipelines:Pipelines) extends Ur
   }
   
   /**
-    * Retrives a document.
+    * Retrieves a document. If the document is still current, the document is not transmitted again and doc is returned.
     */
   def getDoc[A:RootJsonFormat](doc:RevedDocument[A]):Future[RevedDocument[A]] = {
     val p = pipeline[RevedDocument[A]](etag = Some(doc.rev))
